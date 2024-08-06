5f1146937af3d3123e45fc149330603455fb9f4a
FABRICIO ALTAFINI TEODORO <fabricio.ateodoro@senacsp.edu.br>
2024-08-06 16:43:56 -0300

    teste git

--- aprendendocsharp/Program.cs
+++ aprendendocsharp/Program.cs
@@ -10,6 +10,8 @@ namespace aprendendocsharp
     {
         static void Main(string[] args)
         {
+            Console.Write("ola");
+            Console.WriteLine("tudo");
         }
     }
 }
